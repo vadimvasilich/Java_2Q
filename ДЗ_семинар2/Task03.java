fe4997b35b13f4b172b9212d87c1621128678d29
Vadim <3906120@mail.ru>
2023-04-22 11:00:30 +0300

    V_2 Замените все продукты в поваренной книге Васи и выведите их построчно на экран.

--- ДЗ_семинар2/Task03.java
+++ ДЗ_семинар2/Task03.java
@@ -36,32 +36,43 @@ import java.util.*;
 public class Task03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.printf("Введите число строк в книге: ");
+        System.out.printf("Введите число строк в книге: "); // Читаем количество строк книги
         int n = sc.nextInt();
         sc.nextLine();
-        List<String> recipesList = new ArrayList<>();
-        for (int i = 0; i < n; i ++) {
+
+        // Заполняем массив строк книги
+        List<String> recipesList = new ArrayList<>(); 
+        for (int i = 0; i < n; i ++) { 
             System.out.printf("Введите строку " + (i+1) + " = " );
             recipesList.add(sc.nextLine());
         }
+
+        // Читаем количество продуктов, на которые есть аллергия
         System.out.printf("Введите число замен продуктов: ");
         int m = sc.nextInt();
         sc.nextLine();
+
+        // Заполняем массив замен продуктов
         List<String> product_1 = new ArrayList<>(); 
         List<String> product_2 = new ArrayList<>(); 
-        for (int i = 0; i < m; i ++) {
+        for (int i = 0; i < m; i ++) { 
             System.out.printf("Введите продукт " + (i+1) + " и его замену через дефис: ");
             String[] prods = sc.nextLine().split("-");
             product_1.add(prods[0].trim());
             product_2.add(prods[1].trim());
         }
+        
         sc.close();
+        
+        // Заменяем сторочные символы на заглавные
         for (int i = 0, size = product_1.size(); i < size; i ++) {
             product_1.add(product_1.get(i).substring(0, 1).toUpperCase()
             + product_1.get(i).substring(1));
             product_2.add(product_2.get(i).substring(0, 1).toUpperCase()
             + product_2.get(i).substring(1));
         }
+
+        // Создаём массив конечного результата
         List<String> recipesListNew = new ArrayList<>();
         for (int i = 0; i < recipesList.size(); i ++) {
             String recipe = recipesList.get(i);
@@ -69,7 +80,56 @@ public class Task03 {
                 recipe = recipe.replaceAll(product_1.get(j), product_2.get(j));
             recipesListNew.add(recipe);
         }
+
+        // Печатаем результат
         for (String recipe : recipesListNew)
             System.out.println(recipe);
     }
 }
+
+/*
+public class Task03 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    // Читаем количество строк книги
+    int n = scanner.nextInt();
+    scanner.nextLine();
+    
+    // Заполняем массив строк книги
+    String[] book = new String[n];
+    for (int i = 0; i < n; i++) {
+    book[i] = scanner.nextLine();
+    }
+    
+    // Читаем количество продуктов, на которые есть аллергия
+    int m = scanner.nextInt();
+    scanner.nextLine();
+    
+    // Создаем LinkedHashMap для хранения пар продуктов для замены
+    LinkedHashMap replacements = new LinkedHashMap();
+    
+    // Заполняем LinkedHashMap парами продуктов для замены
+    for (int i = 0; i < m; i++) {
+    String[] pair = scanner.nextLine().split(" - ");
+    replacements.put(pair[0], pair[1]);
+    }
+    
+    // Формируем текст книги с учетом замены продуктов
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+    String line = book[i];
+    for (Map.Entry entry : replacements.entrySet()) {
+    String product = entry.getKey();
+    String replacement = entry.getValue();
+    line = line.replace(product, replacement);
+    }
+    result.append(line);
+    result.append("n");
+    }
+    
+    // Выводим результат
+    System.out.println(result.toString());
+    }
+    }
+*/
