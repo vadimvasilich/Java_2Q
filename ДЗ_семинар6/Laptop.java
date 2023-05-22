246fc82be3c2190d1fa41661c3f4c66e0f9ec9e1
Vadim <3906120@mail.ru>
2023-05-22 13:53:37 +0300

    Ноутбук для магазина техники - выделить поля и методы. Реализовать в java. Создать множество ноутбуков.

--- ДЗ_семинар6/Laptop.java
+++ ДЗ_семинар6/Laptop.java
@@ -8,12 +8,14 @@ public class Laptop {
     String os;
 
     public void getInfo() {
-        System.out.println(this.model + ":\n\tЦвет: " + this.color + "\n\tОЗУ: " + this.ramGB + "\n\tОбъем ЖД: " + this.romGB + "\n\tОперационная система: " + this.os + "\n");
+        System.out.println(this.model + ":\n\tЦвет: " + this.color + "\n\tОЗУ: " + this.ramGB + 
+        "\n\tОбъем ЖД: " + this.romGB + "\n\tОперационная система: " + this.os + "\n");
     }
 
     @Override
     public String toString() {
-        return "Laptop [model=" + this.model + ", color=" + this.color + ", ram=" + this.ramGB + ", rom = " + this.romGB + ", os=" + this.os + "]";
+        return "Laptop [model=" + this.model + ", color=" + this.color + ", ram=" + this.ramGB + ", 
+        rom = " + this.romGB + ", os=" + this.os + "]";
     }
 
     public Laptop() {
